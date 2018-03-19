03d705a0fa9e81068dbc40fc12108f3395e473c9
vitaliy kotov <v.kotov@corp.badoo.com>
2018-03-19 21:38:37 +0300

    remove hushmap and set capabilities in setup | we don't need no education

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -4,27 +4,12 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CoreTestCase extends TestCase
 {
     protected AppiumDriver driver;
-
     private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
-
-    protected static Map<String, String> capabilities;
-    static {
-        capabilities = new HashMap<String, String>();
-        capabilities.put("platformName", "Android");
-        capabilities.put("deviceName", "AndroidTestDevice");
-        capabilities.put("platformVersion", "8.0");
-        capabilities.put("automationName", "Appium");
-        capabilities.put("appPackage", "org.wikipedia");
-        capabilities.put("appActivity", ".main.MainActivity");
-    }
 
     @Override
     protected void setUp() throws Exception
@@ -32,11 +17,12 @@ public class CoreTestCase extends TestCase
         super.setUp();
 
         DesiredCapabilities DesiredCapabilities = new DesiredCapabilities();
-        for (Map.Entry capability_entry: capabilities.entrySet()) {
-            String name_of_capability = capability_entry.getKey().toString();
-            String value_of_capability = capability_entry.getValue().toString();
-            DesiredCapabilities.setCapability(name_of_capability, value_of_capability);
-        }
+        DesiredCapabilities.setCapability("platformName", "Android");
+        DesiredCapabilities.setCapability("deviceName", "AndroidTestDevice");
+        DesiredCapabilities.setCapability("platformVersion", "8.0");
+        DesiredCapabilities.setCapability("automationName", "Appium");
+        DesiredCapabilities.setCapability("appPackage", "org.wikipedia");
+        DesiredCapabilities.setCapability("appActivity", ".main.MainActivity");
 
         driver = new AndroidDriver(new URL(AppiumURL), DesiredCapabilities);
     }
