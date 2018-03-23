d486eb05d18cee0a1a724c0ef2a6b3f512a78545
vitaliy kotov <v.kotov@corp.badoo.com>
2018-03-23 21:24:58 +0300

    refactoring testCancelSearch

--- src/lib/ui/SearchPageObject.java
+++ src/lib/ui/SearchPageObject.java
@@ -8,6 +8,7 @@ public class SearchPageObject extends MainPageObject
     private static final String
             SEARCH_INIT_ELEMENT = "//*[contains(@text,'Search Wikipedia')]",
             SEARCH_INPUT = "//*[contains(@text,'Search…')]",
+            SEARCH_CANCEL_BUTTON = "org.wikipedia:id/search_close_btn",
             SEARCH_RESULT_BY_SUBSTRING_TPL = "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']";
 
     public SearchPageObject(AppiumDriver driver)
@@ -26,6 +27,21 @@ public class SearchPageObject extends MainPageObject
     {
         this.waitForElementAndClick(By.xpath(SEARCH_INIT_ELEMENT), "Cannot find and click search init element.", 5);
         this.waitForElementPresent(By.xpath(SEARCH_INIT_ELEMENT), "Cannot find search input after clicking search init element.");
+    }
+
+    public void waitForCancelSearchButtonToAppear()
+    {
+        this.waitForElementPresent(By.id(SEARCH_CANCEL_BUTTON), "Cannot find search button!", 5);
+    }
+
+    public void waitForCancelSearchButtonToDisappear()
+    {
+        this.waitForElementNotPresent(By.id(SEARCH_CANCEL_BUTTON), "Search button still present.!", 5);
+    }
+
+    public void clickCancelSearch()
+    {
+        this.waitForElementAndClick(By.id(SEARCH_CANCEL_BUTTON), "Cannot find and click cancel search button.", 5);
     }
 
     public void typeSearchLine(String search_line)
