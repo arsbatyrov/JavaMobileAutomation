e17802b1f3179db41088b4075979f32ab9cbc07d
vitaliy kotov <v.kotov@corp.badoo.com>
2018-04-15 04:06:06 +0300

    add platform switcher: android and ios values

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -9,6 +9,9 @@ import java.net.URL;
 
 public class CoreTestCase extends TestCase
 {
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
+
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
@@ -17,14 +20,7 @@ public class CoreTestCase extends TestCase
     {
         super.setUp();
 
-        DesiredCapabilities DesiredCapabilities = new DesiredCapabilities();
-        DesiredCapabilities.setCapability("platformName", "Android");
-        DesiredCapabilities.setCapability("deviceName", "AndroidTestDevice");
-        DesiredCapabilities.setCapability("platformVersion", "8.0");
-        DesiredCapabilities.setCapability("automationName", "Appium");
-        DesiredCapabilities.setCapability("appPackage", "org.wikipedia");
-        DesiredCapabilities.setCapability("appActivity", ".main.MainActivity");
-
+        DesiredCapabilities DesiredCapabilities = this.getCapabilitiesByPlatformEnv();
         driver = new AndroidDriver(new URL(AppiumURL), DesiredCapabilities);
         this.rotateScreenPortrait();
     }
@@ -50,5 +46,29 @@ public class CoreTestCase extends TestCase
     protected void backgroundApp(int seconds)
     {
         driver.runAppInBackground(seconds);
+    }
+
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception
+    {
+        String platform = System.getenv("PLATFORM");
+        DesiredCapabilities DesiredCapabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            DesiredCapabilities.setCapability("platformName", "Android");
+            DesiredCapabilities.setCapability("deviceName", "AndroidTestDevice");
+            DesiredCapabilities.setCapability("platformVersion", "8.1");
+            DesiredCapabilities.setCapability("automationName", "Appium");
+            DesiredCapabilities.setCapability("appPackage", "org.wikipedia");
+            DesiredCapabilities.setCapability("appActivity", ".main.MainActivity");
+        } else if (platform.equals(PLATFORM_IOS)) {
+            DesiredCapabilities.setCapability("app", "/Users/vitalijkotov/JavaMobileAutomation/apks/Wikipedia.app");
+            DesiredCapabilities.setCapability("platformName", "iOS");
+            DesiredCapabilities.setCapability("platformVersion", "11.3");
+            DesiredCapabilities.setCapability("deviceName", "iPhone SE");
+        } else {
+            throw new Exception("Cannot get run platform from env variable. Platform value: " + platform);
+        }
+
+        return DesiredCapabilities;
     }
 }
